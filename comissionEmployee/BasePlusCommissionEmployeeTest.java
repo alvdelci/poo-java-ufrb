7e9c735f1c0067b5011ee1503755124191f8c568
alvdelci <delcimariosantos@gmail.com>
2021-03-29 21:07:36 -0300

    Metodo main de BasePlusCommissionEmployee

--- comissionEmployee/BasePlusCommissionEmployeeTest.java
+++ comissionEmployee/BasePlusCommissionEmployeeTest.java
@@ -5,22 +5,21 @@ package comissionEmployee;
 
 public class BasePlusCommissionEmployeeTest {
     public static void main(String[] args) {
-        //Instancia objeto CommissionEmployee
-        CommissionEmployee commissionEmployee = new CommissionEmployee("Rafithy", "Haspy", "432-34-6435", 4000, .04);
+        //Instancia objeto CommissionEmployee(firstname, lastName, socialSecurityName, grossSales, commissionRate)
+        CommissionEmployee commissionEmployee = new CommissionEmployee("Rafithy", "Haspy", "432-34-6435", 4000, .4);
         
-        // instancia objeto BasePlusCommissionEmployee
-        BasePlusCommissionEmployee employee = new BasePlusCommissionEmployee(commissionEmployee, 300);
+        // instancia objeto BasePlusCommissionEmployee(commissionEmployee, baseSalary)
+        BasePlusCommissionEmployee employee = new BasePlusCommissionEmployee(commissionEmployee, 600);
 
         // obtem dados base-salaried commission employee data
         System.out.println("Employee information obtained by get methods:");
-        System.out.printf("%n%s %s%n", "First name is", employee.getEmployee().getFirstName());
-        System.out.printf("%s %s%n", "Last name is", employee.getEmployee().getLastName());
-        System.out.printf("%s %s%n", "Social security number is", employee.getEmployee().getSocialSecurityNumber());
-        System.out.printf("%s %.2f%n", "Gross sales is", employee.getEmployee().getGrossSales());
-        System.out.printf("%s %.2f%n", "Commission rate is", employee.getEmployee().getCommissionRate());
+        System.out.printf("%n%s %s%n", "First name is", employee.getFirstName());
+        System.out.printf("%s %s%n", "Last name is", employee.getLastName());
+        System.out.printf("%s %s%n", "Social security number is", employee.getSocialSecurityNumber());
+        System.out.printf("%s %.2f%n", "Gross sales is", employee.getGrossSales());
+        System.out.printf("%s %.2f%n", "Commission rate is", employee.getCommissionRate());
         System.out.printf("%s %.2f%n", "Base salary is", employee.getBaseSalary());
-
-        employee.setBaseSalary(1000);
+        System.out.printf("%s %.2f%n", "Earnings is", employee.earnings());
 
         System.out.printf("%n%s:%n%n%s%n", "Updated employee information obtained by toString", employee.toString());
     } // end main
